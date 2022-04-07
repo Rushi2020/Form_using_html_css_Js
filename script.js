0a195b5884c6d4e02a146c860389e6f1cbd68f33
Rushi <189489rishi@gmail.com>
2022-04-07 21:53:44 +0530

    create form

---
+++ script.js
@@ -0,0 +1,11 @@
+function setSalary(){
+    var salary= document.getElementById("sal").value;
+
+    var salelement=document.getElementById("salValue");
+
+    salelement.innerText=salary;
+
+}
+function ChangeName(){
+    console.log("salValue")
+}
